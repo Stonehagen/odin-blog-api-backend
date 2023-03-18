e166d03e92468c84ddac240b010d3273768820e0
Stonehagen <steinhagen@mailbox.org>
2023-03-18 13:10:45 +0100

    setup mongoose

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import 'dotenv/config';
+
+// eslint-disable-next-line operator-linebreak
+const mongoDB = process.env.MONGODB_URI;
+mongoose.connect(mongoDB, { useUnifiedTopology: true, useNewUrlParser: true });
+const db = mongoose.connection;
+// eslint-disable-next-line no-console
+db.on('error', console.error.bind(console, 'mongo connection error'));
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from App');
+  res.json({ message: 'Hello from App' });
 });
 
 // eslint-disable-next-line arrow-body-style
