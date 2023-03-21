cebfb396a3872fdd852d66781728fda8e26f9306
Stonehagen <steinhagen@mailbox.org>
2023-03-21 23:42:10 +0100

    change post schema to have a longer text

--- src/models/post.js
+++ src/models/post.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const PostSchema = new Schema({
-  title: { type: String, required: true, maxLength: 100 },
-  text: { type: String, required: true, maxLength: 100 },
+  title: { type: String, required: true, maxLength: 200 },
+  text: { type: String, required: true, maxLength: 10000 },
   published: { type: Boolean, default: false },
   timestamp: { type: Date, default: Date.now },
   author: { type: Schema.Types.ObjectId, ref: 'User' },
